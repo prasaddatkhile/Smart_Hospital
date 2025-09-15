c4f6a3ed07efeb728fd6b1792154f85588ec4a06
prasaddatkhile <prasaddatkhile02@gmail.com>
2025-09-15 14:32:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# smart_hospital
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/smart_hospital/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smart_hospital
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
